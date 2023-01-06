2d2b89b07d89920d9979578732fbf94af2607b9a
Your Name <you@example.com>
2023-01-06 16:36:00 -0500

    Modified budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
+    const [budget, setBudget] = useState('');
+
+    dispatch({
+        type: 'Edit_Budget',
+        payload: 'budget'
+    })
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <label style={{ display: 'inline'}}>Budget: £
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                step='10'
+                max='20000'
+                value={budget}
+                style={{ size: 10}}
+                onChange={(event) => setBudget(event.target.value)}
+                >
+            </input>
+            </label>
         </div>
     );
 };
