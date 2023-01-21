ed0424cf30d3f60eae6aae3f57802ca6ad579aff
Your Name <you@example.com>
2023-01-21 03:17:43 -0500

    All requirements implemented

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,32 +1,51 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
+const MAXIMUM_BUDGET = 20000;
+
 const Budget = () => {
-    const { dispatch } = useContext(AppContext);
-    const [budget, setBudget] = useState('');
+  const { budget,expenses, currency, dispatch } = useContext(AppContext);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+    }, 0);
+  const onChangeBudgetHandler = (event) => {
+    const budgetValueEntered = Number(event.target.value);
+    if (budgetValueEntered < totalExpenses) {
+        alert("You cannot reduce the budget value lower than the spending.");
+        return;
+    }
+    if (budgetValueEntered > MAXIMUM_BUDGET) {
+      alert('Budget can not exceed ' + currency + ' ' + MAXIMUM_BUDGET);
+      return;
+    }
 
     dispatch({
-        type: 'Edit_Budget',
-        payload: 'budget'
-    })
+      type: 'SET_BUDGET',
+      payload: budgetValueEntered,
+    });
+  };
 
-    return (
-        <div className='alert alert-secondary'>
-            <label style={{ display: 'inline'}}>Budget: £
-            <input
-                required='required'
-                type='number'
-                id='budget'
-                step='10'
-                max='20000'
-                value={budget}
-                style={{ size: 10}}
-                onChange={(event) => setBudget(event.target.value)}
-                >
-            </input>
-            </label>
-        </div>
-    );
+  return (
+    <div
+      className="alert alert-secondary"
+      style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
+    >
+      <div>
+        <label htmlFor="budget">Budget: </label>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span style={{marginLeft: '.5rem', marginRight: '.5rem'}}>{currency}</span>
+        <input
+          id="budget"
+          required="required"
+          type="number"
+          value={budget}
+          step="10"
+          onChange={onChangeBudgetHandler}
+        ></input>
+      </div>
+    </div>
+  );
 };
 
 export default Budget;
